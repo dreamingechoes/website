b67a9b6a4d45f2834f6b11dada783d827b6878a1
Ivan Gonzalez <hi@dreamingecho.es>
2023-02-23 22:45:38 +0100

    Add more projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,7 +1,37 @@
 const projectsData = [
   {
+    title: 'Awesome Mental Health',
+    description: `A curated list of awesome articles, websites and resources about mental health in the software industry.`,
+    imgSrc: '/static/images/projects/awesome-mental-health/logo.jpg',
+    href: 'https://dreamingechoes.github.io/awesome-mental-health/',
+  },
+  {
+    title: 'Fabada Conference',
+    description: `Annual software engineering conference located in Gijón, Asturias.`,
+    imgSrc: '/static/images/projects/fabada-conf/logo.jpg',
+    href: 'https://fabadaconf.com/',
+  },
+  {
+    title: 'Elixir Asturias',
+    description: `Elixir Asturias users group.`,
+    imgSrc: '/static/images/projects/elixir-asturias/logo.jpg',
+    href: 'https://elixirasturias.github.io/',
+  },
+  {
+    title: 'Folks Who Code',
+    description: `Organization which aims for a more rich and diverse tech community.`,
+    imgSrc: '/static/images/projects/folks-who-code/logo.jpg',
+    href: 'https://folkswhocode.org/',
+  },
+  {
+    title: 'Shoegaze Bot',
+    description: `Bot that sends a new tweet every day with a new shoegaze song.`,
+    imgSrc: '/static/images/projects/shoegaze-bot/bot.jpg',
+    href: 'https://twitter.com/ShoegazeBot_',
+  },
+  {
     title: 'Neil deGrasse Tyson Ipsum',
-    description: `A simple Lorem Ipsum with all the most Neil deGrasse Tyson Ipsum famous quotes.`,
+    description: `Simple Lorem Ipsum with all the most Neil deGrasse Tyson famous quotes.`,
     imgSrc: '/static/images/projects/neil-degrasse-tyson-ipsum/tyson-ipsum.jpg',
     href: 'http://neilipsum.pw',
   },
