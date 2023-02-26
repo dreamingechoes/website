7fb326585df396799f2a18a7ced355859a1b2ddc
Ivan Gonzalez <hi@dreamingecho.es>
2023-02-26 04:46:36 +0100

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,23 +15,26 @@ const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;800&display=swap"
-            rel="stylesheet"
-          ></link>
-        </>
-      </Head>
-      {isDevelopment && isSocket && <ClientReload />}
+    <>
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;800&display=swap"
+              rel="stylesheet"
+            ></link>
+          </>
+        </Head>
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
       <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+    </>
   )
 }
