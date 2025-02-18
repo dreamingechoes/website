c7cf062cb3533c79abd292ef094744412aba871a
Ivan Gonzalez <hi@dreamingecho.es>
2025-02-18 14:06:52 +0100

    Update content

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
+  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
